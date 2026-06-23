06a3dcc90833edc173b3802231127c02d9063680
Masad Rayan <masadrayan2002@gmail.com>
2026-06-23 17:34:39 +0600

    Register User Route work done

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,5 +1,30 @@
-const registerUserIntoDB = async (payload: any) => {
+import { prisma } from "../../lib/prisma";
+import AppError from "../../utils/AppError";
+import { CreateUserInterFace } from "./user.interface";
 
+const registerUserIntoDB = async (payload: CreateUserInterFace) => {
+    const {name, email, experience_level, target_role } = payload;
+
+    const isUserExists = await prisma.users.findUnique({
+        where : {
+            email
+        }
+    })
+
+    if (isUserExists) {
+        throw new AppError('User already exists', 400)
+    }
+
+    const user = await prisma.users.create({
+        data : {
+            name,
+            email,
+            experience_level,
+            target_role
+        }
+    })
+
+    return user 
 }
 
 
